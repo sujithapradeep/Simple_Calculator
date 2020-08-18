4f821373d9af0e2395ad2b45d244d8ed62dd724c
sujithapradeep <69863460+sujithapradeep@users.noreply.github.com>
2020-08-18 22:07:35 +0530

    getting input

--- calculator.py
+++ calculator.py
@@ -7,5 +7,8 @@ def multiplication(number_1, number_2):
 def division(number_1, number_2):
 
 def get_input():
+    number_1 = int(input("enter first number:"))
+    number_2 = int(input("enter second number:"))
+
 
 input()
