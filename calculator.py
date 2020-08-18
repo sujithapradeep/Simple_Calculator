695ae04150c89733a883296a595e11048b0d5d09
sujithapradeep <69863460+sujithapradeep@users.noreply.github.com>
2020-08-18 22:31:20 +0530

    function call

--- calculator.py
+++ calculator.py
@@ -19,7 +19,10 @@ def get_input():
     number_2 = int(input("enter second number:"))
 
 
-
+input()
 print("1 - add\t 2 - sub\t 3 - multiply\t 4 - divide")
 a = int(input("enter the option"))
-input()
+if(a == 1):
+    addition(number_1,number_2)
+elif(a == 2):
+    subtraction(number_1,number_2)
